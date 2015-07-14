1be094b8d1e14bc27c066706a34534adb763e73e
jtbenjumea <jt.benjumea@gmail.com>
2015-07-14 07:20:57 +0200

    Entrega P2P 7 v.local

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,11 +14,20 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-	models.Quiz.findAll().then(
-		function(quizes){
-			res.render('quizes/index.ejs', {quizes: quizes});
-		}
-	).catch(function(error) { next(error); })	
+	if (req.query.search) {
+		var param = '%' + req.query.search.replace(/ /,'%') + '%';
+		models.Quiz.findAll({where:["pregunta like?", param]}).then (
+			function(quizes){
+				res.render('quizes/index.ejs', {quizes: quizes});
+			}
+		).catch(function(error) { next(error); })				
+	} else {
+		models.Quiz.findAll().then(
+			function(quizes){
+				res.render('quizes/index.ejs', {quizes: quizes});
+			}
+		).catch(function(error) { next(error); })	
+	}	
 };
 
 
