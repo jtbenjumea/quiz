40450f9ba52336d24d1ec00db60f27be31bcba3a
jtbenjumea <jt.benjumea@gmail.com>
2015-07-15 22:10:18 +0200

    Editar preguntas v.local

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -30,7 +30,6 @@ exports.index = function(req, res) {
 	}	
 };
 
-
 // GET /quizes/:id
 exports.show = function(req, res) {
 	res.render('quizes/show', {quiz: req.quiz});
@@ -69,6 +68,33 @@ exports.create = function(req, res) {
 			} else {
 				// Guarda en DB los campos pregunta y respuesta
 				quiz
+				.save({fields: ["pregunta","respuesta"]})
+				.then(function(){res.redirect('/quizes')})
+			}	
+		}	
+	);
+};
+
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+	var quiz = req.quiz;
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	req.quiz
+	.validate()
+	.then (
+		function(err){
+			if (err){
+				res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+			} else {
+				// Guarda en DB los campos pregunta y respuesta
+				req.quiz
 				.save({fields: ["pregunta","respuesta"]})
 				.then(function(){res.redirect('/quizes')})
 			}	
