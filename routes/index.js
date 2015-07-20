4e1c78c4e87b199e29872edb941df023051a0fd9
jtbenjumea <jt.benjumea@gmail.com>
2015-07-20 22:32:28 +0200

    Autenticacion y sesion v.local

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,22 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 // GET home page
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: []});
 });
 
-// Autoload
+// Autoload de comandos con :quizId
 router.param('quizId', quizController.load);
 
-// Definición de rutas
+// Definición de rutas de sesión
+router.get('/login', sessionController.new);  		// fomulario login
+router.post('/login', sessionController.create);	// crear sesión
+router.get('/logout', sessionController.destroy);	// destruir sesión
+
+// Definición de rutas de /quizes
 router.get('/quizes', 						quizController.index);
 router.get('/quizes/:quizId(\\d+)', 		quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);
