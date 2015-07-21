e388cc371ca7557b13de3d52eb69062c0c2d7f35
jtbenjumea <jt.benjumea@gmail.com>
2015-07-21 18:15:15 +0200

    Moderacion de comentarios v.local

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,8 @@ router.get('/', function(req, res) {
 });
 
 // Autoload de comandos con :quizId
-router.param('quizId', quizController.load);
+router.param('quizId', quizController.load);		// autoload: quizId
+router.param('commentId', commentController.load);	// autoload: commentId
 
 // Definición de rutas de sesión
 router.get('/login', sessionController.new);  		// fomulario login
@@ -29,7 +30,10 @@ router.get('/quizes/:quizId(\\d+)/edit', 	sessionController.loginRequired, quizC
 router.put('/quizes/:quizId(\\d+)',			sessionController.loginRequired, quizController.update);
 router.delete('/quizes/:quizId(\\d+)',		sessionController.loginRequired, quizController.destroy);
 
+// Definición de rutas de comentarios
 router.get( '/quizes/:quizId(\\d+)/comments/new', 	commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',		commentController.create);
+router.get( '/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',		
+											sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
